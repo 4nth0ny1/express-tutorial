170d6350797930cbaa717116df372f2e07d7283a
4nth0ny1 <acatullo4@gmail.com>
2023-08-16 17:52:31 -0500

    get requests

--- src/index.js
+++ src/index.js
@@ -6,3 +6,10 @@ const app = express();
 
 const PORT = 3001;
 app.listen(PORT, () => console.log(`Running Express Server on Port: ${PORT}`));
+
+app.get("/groceries", (req, res) => {
+  res.send([
+    { item: "milk", quantity: 2 },
+    { item: "apples", quantity: 3 },
+  ]);
+});
