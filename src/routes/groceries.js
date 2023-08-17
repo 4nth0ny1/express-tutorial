f84fb522b148edd5110622772402dbc72f3873ae
4nth0ny1 <acatullo4@gmail.com>
2023-08-16 19:19:35 -0500

    added query filter for quanity of groceryList

--- src/routes/groceries.js
+++ src/routes/groceries.js
@@ -5,9 +5,26 @@ const router = Router();
 const groceryList = [
   { id: 1, item: "milk", quantity: 2 },
   { id: 2, item: "apples", quantity: 3 },
+  { id: 3, item: "oranges", quantity: 6 },
+  { id: 4, item: "carrots", quantity: 8 },
+  { id: 5, item: "pizza", quantity: 1 },
 ];
 
+// filter results by quantity
+// http://localhost:3001/api/groceries?quantity=2
 router.get("/groceries", (req, res) => {
+  const { quantity } = req.query;
+  const parsedQuantity = parseInt(quantity);
+
+  if (!isNaN(parsedQuantity)) {
+    const filteredList = groceryList.filter(
+      (item) => item.quantity <= parsedQuantity
+    );
+    res.send(filteredList);
+  } else {
+    res.send(groceryList);
+  }
+
   res.send(groceryList);
 });
 
