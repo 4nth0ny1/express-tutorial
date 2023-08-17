b54c6bd70aad0b0b03a1159c1a4bb3cd57ea8e7e
4nth0ny1 <acatullo4@gmail.com>
2023-08-16 19:33:57 -0500

    added res.cookie and viewed in postman

--- src/routes/groceries.js
+++ src/routes/groceries.js
@@ -12,19 +12,26 @@ const groceryList = [
 
 // filter results by quantity
 // http://localhost:3001/api/groceries?quantity=2
+// router.get("/groceries", (req, res) => {
+//   const { quantity } = req.query;
+//   const parsedQuantity = parseInt(quantity);
+
+//   if (!isNaN(parsedQuantity)) {
+//     const filteredList = groceryList.filter(
+//       (item) => item.quantity <= parsedQuantity
+//     );
+//     res.send(filteredList);
+//   } else {
+//     res.send(groceryList);
+//   }
+
+//   res.send(groceryList);
+// });
+
 router.get("/groceries", (req, res) => {
-  const { quantity } = req.query;
-  const parsedQuantity = parseInt(quantity);
-
-  if (!isNaN(parsedQuantity)) {
-    const filteredList = groceryList.filter(
-      (item) => item.quantity <= parsedQuantity
-    );
-    res.send(filteredList);
-  } else {
-    res.send(groceryList);
-  }
-
+  res.cookie("visited", true, {
+    maxAge: 10000, // expires the cookie
+  });
   res.send(groceryList);
 });
 
